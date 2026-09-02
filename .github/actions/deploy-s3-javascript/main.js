4f031d6f230f933e66223cc791b87a3b3c69d050
claudia <claud.sg.11@gmail.com>
2026-09-02 13:34:49 +1000

    now

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,7 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
-  core.notice("Hello from my github custom actions !.");
+  core.notice("Hello from my github custom actions !!.");
 }
 
 run();
